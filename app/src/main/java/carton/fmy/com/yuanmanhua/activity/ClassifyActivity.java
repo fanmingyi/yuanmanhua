503132a9a01c065eb5230bd19dfb830b3f6c5c8b
fanmingyi <635555698@qq.com>
2016-12-30 17:28:13 +0800

    1.增加分类上啦和下啦加载

--- app/src/main/java/carton/fmy/com/yuanmanhua/activity/ClassifyActivity.java
+++ app/src/main/java/carton/fmy/com/yuanmanhua/activity/ClassifyActivity.java
@@ -1,6 +1,7 @@
 package carton.fmy.com.yuanmanhua.activity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v4.widget.SwipeRefreshLayout;
@@ -12,14 +13,12 @@ import com.chad.library.adapter.base.listener.OnItemChildClickListener;
 
 import java.lang.reflect.Method;
 import java.util.ArrayList;
-import java.util.List;
 
 import carton.fmy.com.yuanmanhua.R;
 import carton.fmy.com.yuanmanhua.adapter.ClassifyAdapter;
 import carton.fmy.com.yuanmanhua.bean.HomeBean;
 import carton.fmy.com.yuanmanhua.customview.MyCollectRecyclerView;
 import carton.fmy.com.yuanmanhua.url.UrlEndInterface;
-import carton.fmy.com.yuanmanhua.utils.CollectUtilt;
 import carton.fmy.com.yuanmanhua.utils.NetUtil;
 import carton.fmy.com.yuanmanhua.utils.SnackbarUtil;
 import io.reactivex.Flowable;
@@ -34,8 +33,6 @@ public class ClassifyActivity extends BaseSwipeActivity {
     private LinearLayoutManager linearLayoutManager;
     //容器适配器
     private ClassifyAdapter classifyAdapter;
-
-
     //判断用户点的是那个分类
     int type;
     //下载地址类
@@ -48,6 +45,7 @@ public class ClassifyActivity extends BaseSwipeActivity {
     private String limit = "20";
     //所有数据
     private ArrayList<HomeBean> dataSet;
+    //网络访问方法->>反射机制拿取
     private Method load;
 
     @Override
@@ -62,6 +60,32 @@ public class ClassifyActivity extends BaseSwipeActivity {
         iniAdapter();
         //初始化数据
         inidata();
+        //初始化监听
+        iniEvent();
+    }
+
+    private void iniEvent() {
+
+        swipeRefreshLayout.setOnRefreshListener(() -> {
+            //保证当前没有上啦
+            if (!classifyAdapter.isLoading()) {
+                netData();
+                //禁止上啦
+                classifyAdapter.setEnableLoadMore(false);
+            }
+
+
+        });
+
+        classifyAdapter.setOnLoadMoreListener(() -> {
+            //保证没有下啦
+            if (!swipeRefreshLayout.isRefreshing()) {
+                netData();
+                //使其能让下啦
+                swipeRefreshLayout.setEnabled(false);
+            }
+        });
+
     }
 
     //初始化数据
@@ -73,7 +97,7 @@ public class ClassifyActivity extends BaseSwipeActivity {
         switch (type) {
             case R.id.menu_classify_end:
                 urlBaseClassifyInterface = NetUtil.getRetrofit().create(UrlEndInterface.class);
-               //获取到其中的方法
+                //获取到其中的方法
                 initMethod();
                 break;
         }
@@ -93,19 +117,49 @@ public class ClassifyActivity extends BaseSwipeActivity {
 
     public void netData() {
 
-
-
-
-        if (urlBaseClassifyInterface != null&& this.load !=null) {
-
+        if (urlBaseClassifyInterface != null && this.load != null) {
             try {
-                Flowable<ArrayList<HomeBean>> loadFlowable = (Flowable<ArrayList<HomeBean>>) this.load.invoke(urlBaseClassifyInterface,"" + page++, limit);
+                Flowable<ArrayList<HomeBean>> loadFlowable = (Flowable<ArrayList<HomeBean>>) this.load.invoke(urlBaseClassifyInterface, "" + page++, limit);
                 loadFlowable.observeOn(AndroidSchedulers.mainThread()).subscribeOn(Schedulers.io()).subscribe(data -> {
-                    this.dataSet = data;
-                    classifyAdapter.setNewData(dataSet);
+                    //如果正在下拉
+                    if (swipeRefreshLayout.isRefreshing()) {
+                        this.dataSet = data;
+                        classifyAdapter.setNewData(dataSet);
+                        swipeRefreshLayout.setRefreshing(false);
+                        //允许上啦
+                        classifyAdapter.setEnableLoadMore(true);
+                        //如果当前是上啦
+                    } else if (classifyAdapter.isLoading() && this.dataSet != null) {
+                        //添加数据
+                        this.dataSet.addAll(data);
+                        //刷新数据
+                        classifyAdapter.notifyDataSetChanged();
+                        //完成
+                        classifyAdapter.loadMoreComplete();
+                        //允许下啦
+                        swipeRefreshLayout.setEnabled(true);
+
+                    } else {
+                        this.dataSet = data;
+                        classifyAdapter.setNewData(dataSet);
+                    }
                 }, e ->
                 {
                     SnackbarUtil.getImgSnackbar(my_recycleView, "暂无数据", Snackbar.LENGTH_LONG, ClassifyActivity.this, -1).show();
+                    //如果正在下拉
+                    if (swipeRefreshLayout.isRefreshing()) {
+
+                        swipeRefreshLayout.setRefreshing(false);
+                        //允许上啦
+                        classifyAdapter.setEnableLoadMore(true);
+                        //如果当前是上啦
+                    } else if (classifyAdapter.isLoading() && this.dataSet != null) {
+                        //完成
+                        classifyAdapter.loadMoreEnd();
+                        //允许下啦
+                        swipeRefreshLayout.setEnabled(true);
+                    }
+
                 });
 
             } catch (Exception e) {
@@ -137,7 +191,6 @@ public class ClassifyActivity extends BaseSwipeActivity {
         my_recycleView.setAdapter(classifyAdapter);
 
 
-
         my_recycleView.addOnItemTouchListener(new OnItemChildClickListener() {
             @Override
             public void onSimpleItemChildClick(BaseQuickAdapter baseQuickAdapter, View view, int i) {
@@ -158,13 +211,14 @@ public class ClassifyActivity extends BaseSwipeActivity {
     private void initView() {
         //展示每个收藏的容器list
         my_recycleView = ((MyCollectRecyclerView) findViewById(R.id.my_recycleView));
-
-
         //下啦刷新
         swipeRefreshLayout = ((SwipeRefreshLayout) findViewById(R.id.swipeRefreshLayout));
+        //下啦交替颜色
+        swipeRefreshLayout.setColorSchemeColors(Color.CYAN, Color.BLUE, Color.GREEN);
 
         //收藏
-       // SnackbarUtil.getImgSnackbar(my_recycleView, "手指选择漫画,右滑动可以删除收藏", Snackbar.LENGTH_INDEFINITE, this, -1).show();
+        // SnackbarUtil.getImgSnackbar(my_recycleView, "手指选择漫画,右滑动可以删除收藏", Snackbar.LENGTH_INDEFINITE, this, -1).show();
+
     }
 
 
